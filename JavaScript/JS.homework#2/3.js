4efc026875278ee8f999f9b6b67664bc0ea36f79
RomanLecter <romasqua2@gmail.com>
2023-10-18 13:20:39 +0200

    Update 3.js

--- JavaScript/JS.homework#2/3.js
+++ JavaScript/JS.homework#2/3.js
@@ -1,12 +1,31 @@
 //3 задача
 
-int sum = 0;
-for(int m = A; m <= B; m++)
-  sum += m;
-System.out.ptint(sum)
+function printNumbers(a, b) {
+  // Проверка на корректность входных данных
 
+  if (typeof a !== "number" || typeof b !== "number") {
+    console.log("A и B должны быть целыми числами.");
+    return;
+  }
+  if (a > b) {
+    console.log("A должно быть меньше или равно B.");
+    return;
+  }
 
-int sum = 1;
-for(int m = A; m <= B; m++)
-  sum *= m;
-System.out.ptint(sum);
+  // Вывод чисел в порядке возрастания или убывания
+
+  if (a < b) {
+    for (let i = a; i <= b; i++) {
+      console.log(i);
+    }
+  } else {
+    for (let i = b; i >= a; i--) {
+      console.log(i);
+    }
+  }
+}
+
+// Тестирование
+
+printNumbers(1, 10);
+printNumbers(10, 1);
