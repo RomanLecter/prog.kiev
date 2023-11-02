509877e12eec7d652009da73f7f0f6c78292264d
RomanLecter <romasqua2@gmail.com>
2023-11-02 15:46:45 +0100

    Update newUserName.js

--- JavaScript/JS.homework#7/newUserName.js
+++ JavaScript/JS.homework#7/newUserName.js
@@ -3,15 +3,29 @@ function createNewUser() {
   const firstName = prompt("Введите имя: ");
   const lastName = prompt("Введите фамилию: ");
 
+  // Запрашиваем дату рождения пользователя
+  const birthday = prompt("Введите дату рождения (dd.mm.yyyy): ");
+
   // Создаём объект пользователя
   const newUser = {
     firstName: firstName,
     lastName: lastName,
+    birthday: birthday,
   };
 
-  // Добавляем в объект пользователя метод getLogin()
-  newUser.getLogin = function() {
-    return this.firstName[0].toLowerCase() + this.lastName.toLowerCase();
+  // Добавляем в объект пользователя метод getAge()
+  newUser.getAge = function() {
+    const today = new Date();
+    const birthDate = new Date(birthday);
+    const age = today.getFullYear() - birthDate.getFullYear();
+    return age;
+  };
+
+  // Добавляем в объект пользователя метод getPassword()
+  newUser.getPassword = function() {
+    return this.firstName[0].toUpperCase() +
+      this.lastName.toLowerCase() +
+      this.birthday.slice(-4);
   };
 
   // Возвращаем объект пользователя
@@ -21,8 +35,17 @@ function createNewUser() {
 // Создаём пользователя
 const user = createNewUser();
 
-// Получаем логин пользователя
-const login = user.getLogin();
+// Получаем возраст пользователя
+const age = user.getAge();
 
-// Выводим логин в консоль
-console.log(login);
+// Получаем пароль пользователя
+const password = user.getPassword();
+
+// Выводим результат работы функции createNewUser()
+console.log(user);
+
+// Выводим возраст пользователя
+console.log("Возраст пользователя: " + age);
+
+// Выводим пароль пользователя
+console.log("Пароль пользователя: " + password);
